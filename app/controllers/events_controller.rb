b8312b01ffbf04de91040dc41e4e2f82fc753f3b
norioaks44 <jrc.katou@gmail.com>
2020-12-10 16:30:26 +0900

    編集コントローラー、ビュー作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,6 +20,28 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+  
+  def edit
+    @event = Event.find(params[:id])
+  end
+  
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to event_path
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @event = Event.find(params[:id])
+    if @event.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
 
   private
 
