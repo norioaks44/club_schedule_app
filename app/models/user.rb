08332e09a7b925192cba89e63595e18af5bf4a1b
norioaks44 <jrc.katou@gmail.com>
2020-12-22 19:19:42 +0900

    外部API設定、googleアカウントでの登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,28 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
 extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :grade
   belongs_to :position
   has_many :event
   has_many :reviews
+
+  has_many :sns_credentials
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+
+    user = User.where(email: auth.info.email).first_or_initialize(
+        name: auth.info.name,
+        email: auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
+  end
 
 end
