7f6fa569518c8b3965de1a777ed8c199434fff8a
norioaks44 <jrc.katou@gmail.com>
2020-12-24 21:53:08 +0900

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 if Rails.env.production?
-  OmniAuth.config.full_host = "ec2-54-199-115-86.ap-northeast-1.compute.amazonaws.com"
+  OmniAuth.config.full_host = "http://ec2-54-199-115-86.ap-northeast-1.compute.amazonaws.com"
 end
