8a64a0f8ca79d5a8d7e7a307fa52ca785eb72946
norioaks44 <jrc.katou@gmail.com>
2020-12-24 22:43:34 +0900

    Delete omniauth.rb

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-# if Rails.env.production?
-#   OmniAuth.config.full_host = "https://ec2-54-199-115-86.ap-northeast-1.compute.amazonaws.com"
-# end
-
-OmniAuth.config.full_host = Rails.env.production? ? 'http://localhost:3000' : 'http://ec2-54-199-115-86.ap-northeast-1.compute.amazonaws.com'
