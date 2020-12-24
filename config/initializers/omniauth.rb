e7cb71b26721bcee39279f86edaab8ff7c884602
norioaks44 <jrc.katou@gmail.com>
2020-12-24 23:34:58 +0900

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,4 @@
 #   provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
 #   provider :identity, :fields => [:name, :email]
 # end
-OmniAuth.config.full_host = Rails.env.production? ? 'http://54.199.115.86' : 'http://localhost:3000'
+OmniAuth.config.full_host = Rails.env.production? ? 'http://ec2-54-199-115-86.ap-northeast-1.compute.amazonaws.com/users/auth/google_oauth2/callback' : 'http://localhost:3000'
