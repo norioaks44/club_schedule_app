fb813a3119fcff1555fcb35fb7e810336a51ec13
norioaks44 <jrc.katou@gmail.com>
2020-12-30 17:06:50 +0900

    カラム追加、コントローラー記述

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_100057) do
+ActiveRecord::Schema.define(version: 2020_12_30_074331) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_12_22_100057) do
     t.text "match_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "match_date", null: false
     t.index ["event_id"], name: "index_reviews_on_event_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
